3c83e848ec336d5148b3fa253d4d306d7f782bbe
parthchauhan18 <130209549+parthchauhan18@users.noreply.github.com>
2023-09-24 21:21:01 -0700

    Add files via upload

---
+++ 2.components/ClassCom.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ClassCom extends React.Component{
+    render(){
+        return <h1>class-com</h1>;
+    }
+}
+
+export default ClassCom;
---
+++ 2.components/Funcom.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Funcom() {
+  return (
+    <h1>Funcom</h1>
+  )
+}
+
